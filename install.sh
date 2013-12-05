65b369a52283d711663a53555d3945add97bc0e8
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2013-12-05 01:10:13 -0800

    Moved to mysql for the server.

--- install.sh
+++ install.sh
@@ -3,104 +3,18 @@
 # Get into root
 sudo su
 
-# Install MongoDB if necessary.
-if [ ! -f /usr/bin/mongos ]; then
+apt-get update -y
 
-  # Add MongoDB to apt
-  apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-  echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | tee /etc/apt/sources.list.d/10gen.list
-  # Update the repo list
-  apt-get -y update
-  # Install latest stable version of mongo
-  apt-get install -y mongodb-10gen
+echo "mysql-server mysql-server/root_password select root" | debconf-set-selections
+echo "mysql-server mysql-server/root_password_again select root" | debconf-set-selections
 
-fi
+apt-get -y install mysql-server
 
-# Get the build helper packages, such as G++, GCC, make, etc.
-apt-get install build-essential -y
-# Git is what will allow us to download source code that use the git VCS.
-# Will be used later to install 
-apt-get install git -y
+mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-# Install Node.js if necessary.
-if [ ! -f /usr/local/bin/node ]; then
+restart mysql
 
-  # Download the Node.js repo.
-  git clone https://github.com/joyent/node.git
-  # Change the directory to the repo's folder.
-  cd node
-  # Check out the latest stable release.
-  git checkout v0.10.22
-  # Compile, and install Node.js
-  ./configure && make && make install
+cat /dev/null > /etc/mysql/my.cnf
+cat /vagrant/configs/my.cnf > /etc/mysql/my.cnf
 
-  cd ..
-  rm -rf node
-
-fi
-
-stop cube-collector
-stop cube-evaluator
-
-rm -f /etc/init/cube-collector.conf
-rm -r /etc/init/cube-evaluator.conf
-
-# We will be saving cube in our home directory.
-cd /home/vagrant
-rm -rf cube
-# Download the Cube repo.
-git clone https://github.com/square/cube.git
-# Change the directory into the repo's folder.
-cd cube
-# Check out the latest stable release.
-git checkout v0.2.12
-# Install Cube's dependencies
-npm install websocket@1.0.8
-npm install
-cd ..
-
-COLLECTOR_FILE=$(cat <<EOF
-#!upstart
-description "A collector for the Cube DBMS"
-author "Salehen Shovon Rahman"
-
-start on startup
-stop on shutdown
-
-env PROGRAM_NAME="cube-collector"
-env NODE_PATH="/usr/local/bin/node"
-env COLLECTOR="/home/vagrant/cube/bin/collector.js"
-
-script
-  echo "Trying to start the collector..."
-  echo \$\$ > /var/run/\$PROGRAM_NAME.pid
-  exec \$NODE_PATH \$COLLECTOR >> /var/log/\$PROGRAM_NAME.sys.log 2>&1
-end script
-EOF)
-
-echo "$COLLECTOR_FILE" > /etc/init/cube-collector.conf
-
-start cube-collector
-
-EVALUATOR_FILE=$(cat <<EOF
-#!upstart
-description "The evaluator for the cube DBMS"
-author "Salehen Shovon Rahman"
-
-start on startup
-stop on shutdown
-
-env PROGRAM_NAME="cube-evaluator"
-env NODE_PATH="/usr/local/bin/node"
-env EVALUATOR="bin/evaluator.js"
-
-script
-  echo \$\$ > /var/run/\$PROGRAM_NAME.pid
-  cd "/home/vagrant/cube"
-  exec \$NODE_PATH \$EVALUATOR >> /var/log/\$PROGRAM_NAME.sys.log 2>&1
-end script
-EOF)
-
-echo "$EVALUATOR_FILE" > /etc/init/cube-evaluator.conf
-
-start cube-evaluator
+restart mysql
