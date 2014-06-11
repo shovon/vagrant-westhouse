84859e0e41fbac3c76a27d60494b2c4d9db9dccd
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2014-06-11 13:26:27 -0700

    Removed MongoDB again

--- install.sh
+++ install.sh
@@ -41,13 +41,6 @@ cp /vagrant/configs/redis-server.conf /etc/init/redis-server.conf
 mkdir /home/vagrant/redis-saves
 start redis-server
 
-# Install MongoDB
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
-echo 'deb http://downloads-distro.mongodb.org/repo/ubuntu-upstart dist 10gen' | sudo tee /etc/apt/sources.list.d/mongodb.list
-sudo apt-get update
-sudo apt-get install mongodb-org
-
 # Install Git
 
 apt-get install git -y
